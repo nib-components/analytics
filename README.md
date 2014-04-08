4afca3f1ac4c4ffa4b14e3ce69a86f2f45bb9769
James Newell <j.newell@nib.com.au>
2014-04-08 16:12:29 +1000

    Updated to work with Universal Analytics

---
+++ README.md
@@ -0,0 +1,23 @@
+# nib-components-analytics
+
+A wrapper for [Google Analytics](https://developers.google.com/analytics/devguides/collection/analyticsjs/) that
+abstracts us from changes to the script.
+
+## Methods
+
+### trackEvent(data)
+
+ * data.category        - Required string - Typically the object that was interacted with (e.g. Slider, Modal, Accordion, Button)
+ * data.action          - Required string - The type of interaction (e.g. Click, Open, Close, Show, Hide)
+ * data.label           - Optional string - Useful for categorizing events (e.g. Previous Button, Close Button)
+ * data.value           - Optional string - Values must be non-negative. Useful to pass counts (e.g. 4 times)
+
+## Usage
+
+    var ga = require('analytics');
+
+    ga.trackEvent({
+      'Slider',
+      'Click',
+      'Previous Button'
+    });
--- index.js
+++ index.js
@@ -1,59 +1,22 @@
+/*global ga*/
 /*
- * Wrapper for the async Google Analytics script
- * https://developers.google.com/analytics/devguides/collection/gajs/methods/gaJSApi_gaq
+ * A wrapper for the Google Analytics script
+ * https://developers.google.com/analytics/devguides/collection/analyticsjs/
+ * https://developers.google.com/analytics/devguides/collection/analyticsjs/events
  */
-
-var debug = require('debug')('GA');
-window._gaq = window._gaq || [];
-
 module.exports = {
 
   /**
-   * The current tracker name
-   * @type {String}
+   * The tracker name
+   * @type  {string}
    */
-  tracker: '',
+  tracker: undefined,
 
   /**
-   * Track a page hit. Optionally pass a URL.
-   * @param {String} url Optional URL to track
-   * @return {Object} self
-   */
-  trackPageView: function(url) {
-    this.push('_trackPageview', _.toArray(arguments));
-    return this;
-  },
-
-  /**
-   * Track a custom event. Used for tracking page events like
-   * clicking on a tab or interacting with the UI
-   * @param  {Object} data Data to pass through to the event
-   * @return {Object} self
-   */
-  trackEvent: function(data) {
-    this.push('_trackEvent', [data.category, data.action, data.label, data.value, data.noninteraction]);
-    return this;
-  },
-
-  /**
-   * Push an array of data to the analytics object
-   * @param  {String}   method   Analytics method name, eg. _trackPageView
-   * @param  {Array}    data     Array of data
-   * @return {Object}   self
-   */
-  push: function(method, data) {
-    data = _.compact(data);
-    method = this.getMethod(method);
-    data.unshift(method);
-    window._gaq.push(data);      
-    debug(data);
-    return this;
-  },
-
-  /**
-   * Set the current tracker. This will be prepended
-   * to method names when pushing data
-   * @param {String} name 
+   * Set the current tracker
+   *
+   * @param   {string}  name                      The tracker name
+   * @return  {object}  self
    */
   setTracker: function(name) {
     this.tracker = name;
@@ -61,97 +24,63 @@ module.exports = {
   },
 
   /**
-   * Get the full method name with the tracker name attached
-   * @param  {String} method
-   * @return {String} Method for the current tracker
+   * Appends the tracker name to the method
+   *
+   * @param  {string} method                      The method name
+   * @return {string}
    */
   getMethod: function(method) {
-    return this.tracker + method;
-  },
-
-  /**
-   * Get the current account number
-   * @param  {Function} callback
-   * @return {Object} self
-   */
-  getAccount: function(callback) {
-    var self = this;
-    this.push(function() {
-      var account = window._gat._getTrackerByName()._getAccount();
-      callback.call(self, account);
-    });
-    return this;
-  },
-
-  /**
-   * Get the current tracker object. Only a callback
-   * is passed in the current tracker will be used.
-   * @param {String} name tracker name.
-   * @param  {Function} callback
-   * @return {Object} self
-   */
-  getTracker: function(name, callback) {
-    var self = this;
-
-    if( arguments.length === 1 ) {
-      callback = name;
-      name = this.tracker;
+    if (this.tracker) {
+      method = this.tracker+'.'+method;
     }
+    return method;
+  },
 
-    this.push(function() {
-      var tracker = window._gat._getTrackerByName(name);
-      callback.call(self, tracker);
-    });
+  /**
+   * Executes a method on the analytics object
+   *
+   * @param   {string}   method                   The method name, eg. create, get, set, send
+   * @param   {Array}    params                   The method parameters
+   * @return  {object}   self
+   */
+  exec: function(method, params) {
 
+    //get the parameters (excluding anything which evaluates to undefined]
+    var p = [this.getMethod(method)];
+    for (var i=0; i<params.length; ++i) {
+      if (typeof params[i] !== 'undefined') {
+        p.push(params[i]);
+      }
+    }
+    
+    //execute the method
+    window.ga.apply(ga, p);
     return this;
   },
 
   /**
-   * Set the account used for the analytics tracking 
-   * @param {String} id Google analytics ID
-   * @return {Object} self
+   * Sends data to the analytics object
+   *
+   * @param   {string}   type                     The data type, eg. pageView, event
+   * @param   {Array}    data                     The data
+   * @return  {object}   self
    */
-  setAccount: function(id){
-    this.push('_setAccount', [id]);
-    return this;
+  send: function(type, data) {
+    return this.exec('send', ['event'].concat(data));
   },
 
   /**
-   * Set a custom variable. There are 5 available slots, the index represents
-   * which slot to use. The scope determines for how long to store the data. 
-   * Possible values are 1 for visitor-level, 2 for session-level, and 3 for page-level.
-   * @param {Object} data
-   * @return {Object} self
+   * Tracks a custom event. Used for tracking page events like clicking on a tab or interacting with the UI.
+   *
+   * @param   {object}  data                      The event data
+   * @param   {string}  data.category
+   * @param   {string}  data.action
+   * @param   {string}  [data.label]
+   * @param   {string}  [data.value]
+   * @return  {object}  self
    */
-  set: function(data){
-    this.push('_setCustomVar', [data.index, data.name, data.value, data.scope]);
-    return this;
-  },
-
-  /**
-   * Unset a variable in one of the slots
-   * @param  {Number|String} index Variable slot
-   * @return {Object} self
-   */
-  unset: function(index) {
-    this.push(['_deleteCustomVar', index]);
-    return this;
-  },
-
-  /**
-   * Get a variable in one of the slots
-   * @param  {Number}   index    Variable slot number
-   * @param  {Function} callback 
-   * @return {Object} self
-   */
-  get: function(index, callback) {
-    var self = this;
-    this.push(function() {
-      var pageTracker = _gat._getTrackerByName();
-      var val = pageTracker._getVisitorCustomVar(index);
-      callback.call(self, val);
-    });
-    return this;
+  trackEvent: function(data) {
+    return this.exec('send', ['event', data.category, data.action, data.label, data.value]);
   }
 
 };
